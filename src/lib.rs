2648d25f7a01580b57afa20b90dd68f17fdaad8c
crumblingstatue <radiantstatue@gmail.com>
2025-03-02 10:46:50 +0100

    Add digits support

--- src/lib.rs
+++ src/lib.rs
@@ -20,12 +20,13 @@ pub trait CharExt {
 
 impl CharExt for char {
     fn is_fw(self) -> bool {
-        matches!(self, 'Ａ'..='Ｚ' | 'ａ'..='ｚ' | '｜' | '／' | FW_SPACE)
+        matches!(self, 'Ａ'..='Ｚ' | 'ａ'..='ｚ' | '０'..='９' | '｜' | '／' | FW_SPACE)
     }
     fn to_fw(self) -> Self {
         match self {
             'A'..='Z' => char::from_u32(self as u32 + FW_UC_ADD).unwrap(),
             'a'..='z' => char::from_u32(self as u32 + FW_LC_ADD).unwrap(),
+            '0'..='9' => char::from_u32(self as u32 + FW_NUM_ADD).unwrap(),
             '|' => '｜',
             '/' => '／',
             ' ' => FW_SPACE,
@@ -37,6 +38,7 @@ impl CharExt for char {
         match self {
             'Ａ'..='Ｚ' => char::from_u32(self as u32 - FW_UC_ADD).unwrap(),
             'ａ'..='ｚ' => char::from_u32(self as u32 - FW_LC_ADD).unwrap(),
+            '０'..='９' => char::from_u32(self as u32 - FW_NUM_ADD).unwrap(),
             '｜' => '|',
             '／' => '/',
             FW_SPACE => ' ',
@@ -71,8 +73,10 @@ impl<T: AsRef<str> + ?Sized> StrExt for T {
     }
 }
 
+const FW_NUM_OFFSET: u32 = 0xFF10;
 const FW_UC_OFFSET: u32 = 0xFF21;
 const FW_LC_OFFSET: u32 = 0xFF41;
+const FW_NUM_ADD: u32 = FW_NUM_OFFSET - '0' as u32;
 const FW_LC_ADD: u32 = FW_UC_OFFSET - 'A' as u32;
 const FW_UC_ADD: u32 = FW_LC_OFFSET - 'a' as u32;
 const FW_SPACE: char = char::from_u32(0x3000).unwrap();
@@ -80,11 +84,19 @@ const FW_SPACE: char = char::from_u32(0x3000).unwrap();
 #[test]
 fn test_sw_to_fw() {
     assert_eq!("hello world".to_fw(), "ｈｅｌｌｏ　ｗｏｒｌｄ");
+    assert_eq!(
+        "hello 1234 world".to_fw(),
+        "ｈｅｌｌｏ　１２３４　ｗｏｒｌｄ"
+    );
 }
 
 #[test]
 fn test_fw_to_sw() {
     assert_eq!("ｈｅｌｌｏ　ｗｏｒｌｄ".to_sw(), "hello world");
+    assert_eq!(
+        "ｈｅｌｌｏ　１２３４　ｗｏｒｌｄ".to_sw(),
+        "hello 1234 world"
+    );
 }
 
 #[test]
@@ -93,4 +105,7 @@ fn test_has_fw() {
     assert!("ｈｅｌｌｏ　ｗｏｒｌｄ".has_fw());
     // Normal looking string with fullwidth space in middle
     assert!("hello　world".has_fw());
+    // Numbers
+    assert!("２".has_fw());
+    assert!("hello １２３４ world".has_fw());
 }
